109df20727d1ef946e8ed26b36b12c5a30eda1e1
Anthony Chu <Anthony Chu@DESKTOP-U9OKJAC.mshome.net>
2021-02-23 19:46:08 +0800

    main func change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -6,8 +6,9 @@ package Lab1;
 public class Library {
 	
 	public static void main(String arg[]) {
-		System.out.println("asdasd");
-		
+		Library lib = new Library();
+		if(lib.someLibraryMethod())
+			System.out.println("asdasd");	
 	}
 	
     public boolean someLibraryMethod() {
